724c9cc27b85c7a2692020fba49898b557f75f38
JupperTV <fmmartyn10@gmail.com>
2024-06-19 07:38:58 +0200

    grammar

--- lua/init.lua
+++ lua/init.lua
@@ -1,5 +1,5 @@
 
 require('telescope').setup {
-	file_ignore_patters = { "^node_modules/", ".cache", "%.o", "%.a", "%.exe", "%.out", "%.class", "%.dll", "%.pdf", "%.mkv", "%.mp4", "%.zip" }
+	file_ignore_patterns = { "^node_modules/", ".cache", "%.o", "%.a", "%.exe", "%.out", "%.class", "%.dll", "%.pdf", "%.mkv", "%.mp4", "%.zip" }
 }
 
